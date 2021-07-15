77c15160c73ba0c3316f1aaeb2eb4d1cdb6bc743
it210137821 <87489655+it21013782@users.noreply.github.com>
2021-07-15 17:59:24 +0000

    Tute 03 completed

--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,17 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int n,i,sum = 0;
+	
+	printf("n -> ");
+	scanf("%d",&n);
+	
+	for(i =1; i<=n; i++){
+		sum = sum + i;
+	}
+	
+	printf("Sum = %d",sum);
+
   return 0;
 }
 
